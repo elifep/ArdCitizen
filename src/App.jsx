1de42072bd08194be652d466f697cda82e183be4
fatih <chessmaster38@hotmail.com>
2024-12-22 15:19:53 +0300

    update home

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import BasvuruEkle from "./pages/BasvuruEkle";
 import BasvuruAlindi from "./pages/BasvuruAlindi";
-import Home from "./pages/home";
+import Home from "../src/pages/Home";
 
 function App() {
   return (
@@ -11,7 +11,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/basvuru-ekle-vatandas" element={<BasvuruEkle />} />
         <Route path="/basarili" element={<BasvuruAlindi />} />
-
       </Routes>
     </Router>
   );
