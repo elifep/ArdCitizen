4437d60127a1c0a528370cd2a6c545f7f2225ca2
elifep <elifep56@gmail.com>
2024-12-27 23:51:39 +0300

    updatee

--- src/components/Process/Case1.jsx
+++ src/components/Process/Case1.jsx
@@ -150,7 +150,7 @@ const Case1 = ({ formData, setFormData, handleInputChange, errors }) => {
                     <input
                         type="text"
                         name="applicantType"
-                        placeholder="Örnek: Vatandaş"
+                        placeholder="Örnek: Vatandaş, Kamu Kurumu..."
                         value={formData.applicantType}
                         onChange={handleInputChange}
                         className="border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-400"
